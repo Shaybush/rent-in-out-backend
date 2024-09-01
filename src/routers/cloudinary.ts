0ffa99a90ee639668f827ad6f75c90dfc6fbf93c
Omri Bitan <79647147+OB-111@users.noreply.github.com>
2024-09-01 13:47:57 +0300

    REN-131
    * add swagger to cloudinary router
    
    * fix cloudinary swagger

--- src/routers/cloudinary.ts
+++ src/routers/cloudinary.ts
@@ -4,6 +4,67 @@ import { deleteImageCloudinary } from '../services/cloudinaryService';
 
 const router = express();
 
+/**
+ * @swagger
+ * /cloudinary/image:
+ *   delete:
+ *     tags:
+ *       - Cloudinary operations
+ *     summary: Must be connected as user
+ *     description: Deletes an image from Cloudinary using its image ID.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               img_id:
+ *                 type: string
+ *                 description: The ID of the image to delete from Cloudinary.
+ *             required:
+ *               - img_id
+ *     responses:
+ *       201:
+ *         description: Image deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: string
+ *                   description: The result from Cloudinary after deletion.
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       description: Error message
+ *                     name:
+ *                       type: string
+ *                       description: Error name
+ *                     http_code:
+ *                       type: number
+ *                       description: HTTP status code
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Couldn't find resource with id
+ */
 router.delete('/image', auth, deleteImageCloudinary);
 
 export default router;
