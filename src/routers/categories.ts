2f61686abeb40d4e51e9b2e582ef41544c46bf87
Shay Bushary <48178609+Shaybush@users.noreply.github.com>
2024-09-03 00:17:51 +0300

    REN-130 (#23)
    * REN-130
    
    * improve description

--- src/routers/categories.ts
+++ src/routers/categories.ts
@@ -16,7 +16,8 @@ const router = express();
  * @swagger
  * /categories:
  *   get:
- *     tags: ['Categories operations']
+ *     tags:
+ *     -  Categories operations
  *     description: Get categories list
  *     parameters:
  *      - in: query
@@ -32,16 +33,13 @@ const router = express();
  *        description: Reverse the data.
  *     responses:
  *       200:
- *         description: Returns the categories filtered list
+ *         description: Returns the categories list.
  *         content:
  *           application/json:
  *               schema:
- *                  type: object
- *                  properties:
- *                      data:
- *                          type: array
- *                          items:
- *                             $ref: "#/components/schemas/category"
+ *                  type: array
+ *                  items:
+ *                    $ref: "#/components/schemas/category"
  *       500:
  *         description: Internal server error
  */
@@ -51,10 +49,12 @@ router.get('/', getCategorylist);
  * @swagger
  * /categories/search:
  *   get:
- *     tags: ['Categories operations']
- *     description: Get categories list
+ *     tags:
+ *     -  Categories operations
+ *     description: Get filtered categories list.
+ *     summary: Must be connected as admin
  *     security:
- *       - apiKeyAuth: []   # This line adds the x-api-key to this specific route
+ *       - apiKeyAuth: []
  *     parameters:
  *      - in: query
  *        name: perPage
@@ -67,29 +67,132 @@ router.get('/', getCategorylist);
  *        schema:
  *          type: string
  *        description: Selected page.
+ *      - in: query
+ *        name: sort
+ *        schema:
+ *          type: string
+ *        description: Choose the element in the categories to be sorted.
+ *      - in: query
+ *        name: reverse
+ *        schema:
+ *          type: string
+ *        description: reverse could be 'yes' for asc order or desc default.
  *     responses:
  *       200:
- *         description: Returns the categories filtered list
+ *         description: Returns the filtered categories list.
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: array
- *                   items:
- *                     $ref: "#/components/schemas/category"
+ *                type: array
+ *                items:
+ *                  $ref: "#/components/schemas/category"
  *       500:
  *         description: Internal server error
  */
 router.get('/search', authAdmin, searchCategories);
 
+/**
+ * @swagger
+ * /categories/count:
+ *   get:
+ *     tags:
+ *     -  Categories operations
+ *     description: Get the categories amount.
+ *     responses:
+ *       200:
+ *         description: Returns the categories amount.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 count:
+ *                   type: number
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/count', countCategories);
 
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     tags:
+ *     -  Categories operations
+ *     description: Create new Category.
+ *     summary: Must be connected as admin
+ *     requestBody:
+ *        description: Categories data body
+ *        required: true
+ *        content:
+ *           application/json:
+ *               schema:
+ *                  type: object
+ *                  required: [ "name", "url_name", "info" ]
+ *                  properties:
+ *                      name:
+ *                          type: string
+ *                          example: "Myname"
+ *                      url_name:
+ *                          type: string
+ *                          example: "url_name"
+ *                      info:
+ *                          type: string
+ *                          example: "info"
+ *     responses:
+ *       200:
+ *         description: Returns new category has been added.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/category"
+ *       400:
+ *         description: Unauthenticated user
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/', authAdmin, categoryControl, addCategory);
 
+/**
+ * @swagger
+ * /categories/{idEdit}:
+ *   put:
+ *     tags:
+ *     -  Categories operations
+ *     description: Edit category by spesific id.
+ *     summary: Must be connected as admin
+ *     responses:
+ *       200:
+ *         description: Edit and return the category with spesfic `id`.
+ *         content:
+ *           application/json:
+ *               schema:
+ *                 $ref: "#/components/schemas/category"
+ *       400:
+ *         description: Unauthenticated user
+ *       500:
+ *         description: Internal server error
+ */
 router.put('/:idEdit', authAdmin, categoryControl, editCategory);
 
+/**
+ * @swagger
+ * /categories/{idDel}:
+ *   delete:
+ *     tags:
+ *     -  Categories operations
+ *     description: Delete Category by id.
+ *     summary: Must be connected as admin
+ *     responses:
+ *       200:
+ *         description: Delete and return the category with spesfic `id`.
+ *         content:
+ *           application/json:
+ *               schema:
+ *                 $ref: "#/components/schemas/category"
+ *       500:
+ *         description: Internal server error
+ */
 router.delete('/:idDel', authAdmin, deleteCategory);
 
 export default router;
